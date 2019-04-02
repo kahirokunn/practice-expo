c62224bb27f8b23cb893699b6effe4bc72c5dafd
k-okina <okinakahiro@gmail.com>
2019-04-03 02:28:12 +0900

    [add] 要素を重ねてみた

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -18,18 +18,58 @@ export default class HomeScreen extends React.Component {
       <View style={{
         flexGrow: 1
       }}>
+
+        <Text style={{
+          fontSize: 50,
+          height: 50,
+          lineHeight: 50,
+          textAlignVertical: 'center',
+          position: 'absolute',
+          backgroundColor: 'blue',
+          zIndex: 1,
+          bottom: 10,
+          right: 0,
+        }}>
+          Hello World
+        </Text>
+
+        <View style={{
+          position: 'absolute',
+          zIndex: 1,
+          right: 10,
+          bottom: 10,
+        }}>
+          <View style={{
+            color: 'black',
+            borderRadius: 50,
+            backgroundColor: 'yellow',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+            <Text style={{
+              fontSize: 50,
+              width: 50,
+              height: 50,
+              lineHeight: 50,
+              textAlign: 'center'
+            }}>
+              +
+            </Text>
+          </View>
+        </View>
+
         <ScrollView>
           {[...Array(100)].map((_, i) => (
             <View key={i} style={{
               flexGrow: 1,
-              justifyContent: "space-around",
+              justifyContent: 'space-around',
               backgroundColor: 'powderblue',
               minHeight: 300,
-              alignItems: "center",
+              alignItems: 'center',
             }}>
-              <ColorSquare style={{ backgroundColor: "blue" }}/>
-              <ColorSquare style={{ backgroundColor: "green" }}/>
-              <ColorSquare style={{ backgroundColor: "red" }}/>
+              <ColorSquare style={{ backgroundColor: 'blue' }}/>
+              <ColorSquare style={{ backgroundColor: 'green' }}/>
+              <ColorSquare style={{ backgroundColor: 'red' }}/>
             </View>
           ))}
         </ScrollView>
